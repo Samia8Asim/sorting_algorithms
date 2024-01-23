fea3e2d4f54c2c042458f570d2158afcf2c69438
rawan-omer <rawanomer0119@gmail.com>
2024-01-23 11:46:52 +0000

    alx programming

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,29 +8,34 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node, *cur = (*list)->next;
+	listint_t *node, *cur, *back;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
         	return;
+
+	cur = (*list)->next;
 
 	while (cur)
 	{
 		node = cur;
 		while (node->prev && node->n < node->prev->n)
 		{
-			if (node->next)
-				node->next->prev = node->prev;
+			back = node->prev;
+			back->next = node->next;
 
-			node->prev->next = node->next;
-			node->next = node->prev;
-			node->prev = node->prev->prev;
+			if (node->next)
+				node->next->prev = back;
+
+			node->next = back;
+			node->prev = back->prev;
+			back->prev = node;
 
 			if (node->prev)
 				node->prev->next = node;
 			else
 				*list = node;
 
-			print_list(*list);
+			print_list((const listint_t *)*list);
         	}
 		cur = cur->next;	
 	}
